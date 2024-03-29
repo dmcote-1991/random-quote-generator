f790890a7bc6a83eef0a3ad4aff676dcc0b92cd1
David Cote <dmc1991@icloud.com>
2024-03-29 08:37:59 -0400

    Added an array of quotes as objects.

---
+++ js/script.js
@@ -0,0 +1,63 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+    quote: `"Programming isn't about what you know; it's about what you can figure out."`,
+    source: `Chris Pine`,
+    citation: `Learn to Program`,
+    year: `2021`
+  },
+  {
+    quote: `"The most damaging phrase in the language is 'We've always done it this way!'"`,
+    source: `Grace Murray Hopper`,
+    citation:`Information Week (interview)`,
+    year: `1987`
+  },
+  {
+    quote: `Everybody should learn to program a computer because it teaches you how to think.`,
+    source: `Steve Jobs`,
+    citation: `On an interview with Robert Cringley`,
+    year: `1995`
+  },
+  {
+    quote: `Experience is the name everyone gives to their mistakes.`,
+    source: `Oscar Wilde`,
+    citation: `Lady Windermere's Fan`,
+    year: `1892`
+  },
+  {
+    quote: `Code is like humor. When you have to explain it, it's bad.`,
+    source: `Cory House`,
+    citation: `Twitter`,
+    year: `2013`
+  }
+]
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
